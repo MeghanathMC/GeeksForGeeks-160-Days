7be134ce81eddcde840898d0b160d55bae98b95f
Meghanath Reddy M C <116367208+MeghanathMC@users.noreply.github.com>
2024-11-22 20:19:12 +0530

    day 3 done

--- Day3.java
+++ Day3.java
@@ -1,3 +1,14 @@
 class Day3{
-    
+    public void reverseArray(int arr[]) {
+        // code here
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int temp=arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+        }
+    }
 }
